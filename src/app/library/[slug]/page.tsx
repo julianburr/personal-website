0f316002b884bef5cda22e66cc69bd0191cfe739
Julian Burr <hello@julianburr.de>
2024-10-26 16:57:18 +1000

    wip

--- src/app/library/[slug]/page.tsx
+++ src/app/library/[slug]/page.tsx
@@ -1,10 +1,8 @@
-import dayjs from "dayjs";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
 import { getPageFromPath } from "@/utils/getPageFromPath";
 import { getPagesFromPath } from "@/utils/getPagesFromPath";
-import { getTimeToRead } from "@/utils/getTimeToRead";
 
 import type { Metadata } from "next";
 
@@ -31,11 +29,15 @@ export default async function LibraryDetailsPage({ params }: any) {
   return (
     <>
       <p className="font-heading p-0 leading-[1.2]">
-        <Link href="/library">Library</Link> —{" "}
-        {dayjs(page?.meta?.date).format("MMMM D, YYYY")} —{" "}
-        {getTimeToRead(page?.content?.raw)} min read
+        <Link href="/library">Library</Link> — {page?.meta?.author}
       </p>
       <h1 className="p-0 mt-1 mb-6">{page?.meta?.title}</h1>
+
+      {page?.meta?.cover && (
+        <div className="flex mb-6">
+          <img src={page?.meta?.cover} className="h-[14rem] w-auto" />
+        </div>
+      )}
 
       <div
         className="details"
